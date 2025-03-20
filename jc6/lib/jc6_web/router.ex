d49e68792ba04ced24915b2211dc1af6aa109b3a
treejamie <me@treejamie.com>
2025-03-20 08:45:37 +0000

    the action dance

--- jc6/lib/jc6_web/router.ex
+++ jc6/lib/jc6_web/router.ex
@@ -7,7 +7,7 @@ defmodule Jc6Web.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {Jc6Web.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
